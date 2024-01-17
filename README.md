740ae4581f3da2f1818d6e7f2f1b7b10d93448fd
o2sa <o2sama1@outlook.sa>
2024-01-17 22:53:30 +0300

    actions and protected routes section

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cofb344e0b911263f4e338368
+# co5cc482e8e3d88fc08865c34
 
 Warning: Vite enforces using jsx syntax inside jsx/tsx files, so it will complain about that. Solution: rename `.js` files to `.jsx` :)
 
--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,31 +1,31 @@
 import React from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, useSearchParams, useLoaderData } from "react-router-dom"
+import { getVans } from "../../api"
+
+export function loader() {
+    return getVans()
+}
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
-    const [vans, setVans] = React.useState([])
+    const [error, setError] = React.useState(null)
+    const vans = useLoaderData()
 
     const typeFilter = searchParams.get("type")
-
-    React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
 
     const displayedVans = typeFilter
         ? vans.filter(van => van.type === typeFilter)
         : vans
-    
-    /**
-     * Mini-challenge: change the absolute path in the Link below
-     * to a relative path. There's also one still in the HostVans.jsx
-     * file that you should change, too.
-     */
-    
+
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
+            <Link
+                to={van.id}
+                state={{
+                    search: `?${searchParams.toString()}`,
+                    type: typeFilter
+                }}
+            >
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
@@ -45,6 +45,10 @@ export default function Vans() {
             }
             return prevParams
         })
+    }
+    
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
     }
 
     return (
